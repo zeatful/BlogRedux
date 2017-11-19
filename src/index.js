0cf5f7bd7ba7770c3aa136502190ff01c9a56510
ericfoster <eric_foster73@hotmail.com>
2017-11-19 00:13:18 -0500

    add axios and redux-promise for use with the fetch_posts action handler

--- src/index.js
+++ src/index.js
@@ -3,30 +3,19 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index';
 
-class Hello extends React.Component {
-  render() {
-    return <div>Hello!</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Goodbye!</div>
-  }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
   <BrowserRouter>
     <div>
-      <Route path="/hello" component={Hello}/>
-      <Route path="/goodbye" component={Goodbye}/>
+      <Route path="/" component={PostsIndex}/>
     </div>
   </BrowserRouter>
 </Provider>, document.querySelector('.container'));
