56abcb989e5ef53c426b1c880e004c1dd916f2fe
ericfoster <eric_foster73@hotmail.com>
2017-11-24 00:52:17 -0500

    add omit action for removing deleted posts from the application state

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import {fetchPost} from '../actions';
+import {fetchPost, deletePost} from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -9,6 +9,12 @@ class PostsShow extends Component {
     this
       .props
       .fetchPost(id);
+  }
+  onDeleteClick() {
+    const {id} = this.props.match.params;
+    this
+      .props
+      .deletePost(id, () => this.props.history.push('/'));
   }
   render() {
     const {post} = this.props;
@@ -20,6 +26,13 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this
+          .onDeleteClick
+          .bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -37,4 +50,4 @@ function mapStateToProps({
   };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
@@ -13,6 +13,8 @@ export default function (state = {}, action) {
         ...state,
         [action.payload.data.id]: action.payload.data
       };
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
